cd03031805f710a310eb787da7bf96f332c3199f
Juventz ARISTIL MATHURIN <jaristil@made-f0Dr9s15.clusters.42paris.fr>
2024-10-22 18:46:06 +0200

    ex00 working and tested

--- Piscine_0 OOB/ex00/render.py
+++ Piscine_0 OOB/ex00/render.py
@@ -4,8 +4,12 @@ import settings
 
 def render_template(file_path):
 
+    surname = "The Hitchhiker's Guide"
+    age = 42
+    profession = "No one knows"
+
     if not file_path.endswith('.template'):
-        raise TypeError(f' {file_path} file is not a template')
+        raise TypeError(f' {file_path} file is not a .template')
 
     output = file_path.replace('.template', '.html')
 
@@ -18,23 +22,42 @@ def render_template(file_path):
 
     rendered_content = template.replace('{name}', settings.name)
 
+    html_structure = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> {settings.name}'s CV </title>
+</head>
+<body>
+    <header>
+        <h1> {settings.name} </h1>
+        <p> {surname} </p>
+        <p> Age: {age} </p>
+        <p> Profession: {profession} </p>
+    </header>
+    <section>
+{rendered_content}
+    </section>
+</body>
+</html>"""
+
     with open(output, 'w') as file:
-        file.write(rendered_content)
+        file.write(html_structure)
+
+    print(f'File {output} has been created')
 
 
 def main():
     try:
         if len(argv) != 2:
-            print('Usage: python3 render.py <file_path>"')
+            print('Usage: python3 render.py <.template>')
         else:
             render_template(argv[1])
 
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
         return
-
-    finally:
-        print('Rendering complete')
 
 
 if __name__ == '__main__':
