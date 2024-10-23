10224823810590670398d9c171f29907cc303832
Juventz ARISTIL MATHURIN <jaristil@paul-f4Br7s3.clusters.42paris.fr>
2024-10-23 15:25:03 +0200

    ex01 done and ok

--- Piscine_0 OOB/ex01/intern.py
+++ Piscine_0 OOB/ex01/intern.py
@@ -1,11 +1,40 @@
+class Coffee:
+    def __str__(self):
+        return "This is the worst coffee you ever tasted."
+
+
 class Intern:
-    # default Builder taking character chain as a parameter and assigning it to a Name attribute
-    def __init__(self, Name="My name? I’m nobody, an intern, I have no name."):
+    def __init__(self, Name="My name? I'm nobody, an intern, I have no name."):
         self.Name = Name
 
     # method that returns the name attribute
     def __str__(self):
         return self.Name
 
+    def work(self):
+        raise Exception("I'm just an intern, I can't do that...")
 
-    
+    def make_coffee(self):
+        return Coffee()
+
+
+def main():
+    try:
+        intern_named = Intern()
+        intern_nonamed = Intern("Mark")
+
+        print(intern_named)
+        print(intern_nonamed)
+        print("---------------")
+
+        print(intern_nonamed.make_coffee())
+        print("---------------")
+        print(intern_named.work())
+
+    except Exception as e:
+        print(e)
+        return
+
+
+if __name__ == "__main__":
+    main()
