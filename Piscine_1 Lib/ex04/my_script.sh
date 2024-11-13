41247461ea4cd7956c53ba1ba461a3e84509197c
juvaris <juvaris@My-Virtual-Machine.myguest.virtualbox.org>
2024-11-13 20:00:16 +0100

    finish 04

--- Piscine_1 Lib/ex04/my_script.sh
+++ Piscine_1 Lib/ex04/my_script.sh
@@ -1,11 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
 python3 -m venv django_venv
 
 source django_venv/bin/activate
 
 pip install --upgrade pip
-pip install requirements.txt
+pip install -r requirement.txt
 
 echo "Django environment created and requirements installed"
-exec bash
