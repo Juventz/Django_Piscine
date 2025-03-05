2255ee57e5cba4f7f7d3dd88d9b37d98fdd69fa5
Juventz ARISTIL MATHURIN <jaristil@paul-f4Br4s4.clusters.42paris.fr>
2025-03-05 14:09:49 +0100

    day base finished and fixed

--- Piscine_1 Base/d05/ex03/views.py
+++ Piscine_1 Base/d05/ex03/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
 
-
-# Create your views here.
-def generate_shade(color, num_shades=50):
+def generate_shades(num_shades=50):
     shades = []
     for i in range(num_shades):
-        shades_value = int(255 - (i * 255 // (num_shades - 1)))
-        if color == "red":
-            shade = f'rgb({shades_value}, 0, 0)'
-        elif color == "green":
-            shade = f'rgb(0, {shades_value}, 0)'
-        elif color == "blue":
-            shade = f'rgb(0, 0, {shades_value})'
-        shades.append(shade)
+        shade_value = int(255 - (i * 255 // (num_shades - 1)))
+        shades.append({
+            'red': f'rgb({shade_value}, 0, 0)',
+            'green': f'rgb(0, {shade_value}, 0)',
+            'blue': f'rgb(0, 0, {shade_value})',
+        })
     return shades
 
-
 def color(request):
-    colors = ['red', 'green', 'blue']
-    color_shades = {
-        color: generate_shade(color) for color in colors
-    }
-
+    color_shades = generate_shades()
     return render(request, 'color.html', {'color_shades': color_shades})
